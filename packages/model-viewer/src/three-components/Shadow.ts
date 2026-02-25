f04e2e117e45583b39de5917c3f27d6c1ff14d25
jwan <jonathan.wanty@solunea.fr>
2026-02-25 17:54:40 +0100

    fix: increase shadow floor and camera frustum scale factors to prevent clipping on larger models

--- packages/model-viewer/src/three-components/Shadow.ts
+++ packages/model-viewer/src/three-components/Shadow.ts
@@ -86,11 +86,11 @@ export class Shadow extends Object3D {
     if (side === 'bottom') {
       this.floor.rotation.x = -Math.PI / 2;
       this.floor.position.set(center.x, min.y, center.z);
-      this.floor.scale.set(this.size.x * 20, this.size.z * 20, 1);
+      this.floor.scale.set(this.size.x * 50, this.size.z * 50, 1);
     } else {
       this.floor.rotation.x = 0;
       this.floor.position.set(center.x, center.y, min.z);
-      this.floor.scale.set(this.size.x * 20, this.size.y * 20, 1);
+      this.floor.scale.set(this.size.x * 50, this.size.y * 50, 1);
     }
 
     this.updateLightPosition();
@@ -134,12 +134,12 @@ export class Shadow extends Object3D {
     this.light.target.updateMatrixWorld();
 
     // Fit shadow camera frustum to the bounding box
-    const halfSize = this.maxDimension * 10;
+    const halfSize = this.maxDimension * 25;
     this.light.shadow.camera.left = -halfSize;
     this.light.shadow.camera.right = halfSize;
     this.light.shadow.camera.top = halfSize;
     this.light.shadow.camera.bottom = -halfSize;
-    this.light.shadow.camera.far = radius * 20;
+    this.light.shadow.camera.far = radius * 50;
     this.light.shadow.camera.updateProjectionMatrix();
   }
 
